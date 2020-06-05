529fc50370010cf762f9b162deee98ee7965303e
Benjamin Dawson-Bruce <benjamindawson-bruce@benjamins-mbp.home>
2020-06-05 18:31:15 +0100

    onclick events added to call rock off fucntion

--- functions.js
+++ functions.js
@@ -1,34 +1,35 @@
 function rockoff(choice){
     var result = Math.floor(Math.random() * 3);
-    console.log(result);
+    //console.log(result);
     var challenge;
+    var outcome;
     if (result === 0){
         challenge = "Paper"
     }else if(result === 1){
         challenge = "Scissors"
-    }else{
+    }else if(result === 2){
         challenge = "Socks"
     }
     if (challenge === choice){
-        return `${choice} vs ${challenge}. It's a draw`;
+        outcome =  `${choice} vs ${challenge}. It's a draw`;
     }else if (choice === "Paper" && challenge === "Socks"){
-        return `${choice} vs ${challenge}. ${choice} beats ${challenge}. You win`;
+        outcome =  `${choice} vs ${challenge}. ${choice} beats ${challenge}. You win`;
     }else if (choice === "Paper" && challenge === "Scissors"){
-        return `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
+        outcome =  `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
     }else if (choice === "Scissors" && challenge === "Socks"){
-        return `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
+        outcome =  `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
     }else if (choice === "Scissors" && challenge === "Paper"){
-        return `${choice} vs ${challenge}.  ${choice} beats ${challenge}. You win`;
+        outcome =  `${choice} vs ${challenge}.  ${choice} beats ${challenge}. You win`;
     }else if (choice === "Socks" && challenge === "Paper"){
-        return `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
+        outcome =  `${choice} vs ${challenge}.  ${challenge} beats ${choice} . You Lose`;
     }else if (choice === "Socks" && challenge === "Scissors"){
-        return `${choice} vs ${challenge}.  ${choice} beats ${challenge}. You win`;
-    }else{
-        return "stay tuned"
+        outcome =  `${choice} vs ${challenge}.  ${choice} beats ${challenge}. You win`;
     }
+
+   document.getElementById("result").innerHTML = outcome;
 
 }
 
-console.log(rockoff("Scissors"));
+//console.log(rockoff("Scissors"));
 
 //console.log("hello");
